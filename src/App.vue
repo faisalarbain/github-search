37b99cba4c8a77c25538897d8f5e9b6ad9d0301e
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-29 02:25:42 +0800

    add icon to search

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <div class="container">
       <div class="section">
         <router-view />
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <input
-      @keypress.enter="submit"
-      type="text"
-      class="input"
-      v-model="keyword"
-      :class="{ 'is-danger': error }"
-    />
+  <div class="field ">
+    <div class="control has-icons-right">
+      <input
+        @keypress.enter="submit"
+        type="text"
+        class="input"
+        v-model="keyword"
+        :class="{ 'is-danger': error }"
+        placeholder="Search"
+      />
+      <span class="icon is-small is-right">
+        <icon name="search" />
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+import Icon from './Icon'
 export default {
+  components:{
+    Icon
+  },
   data() {
     return {
       keyword: "",
