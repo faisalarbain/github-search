0c7c499f7d9da48391e0ae24afa1d0c3cbaa5c56
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-28 23:00:30 +0800

    prepare test

---
+++ tests/unit/search.spec.js
@@ -0,0 +1,13 @@
+// import { expect } from "chai";
+// import { shallowMount } from "@vue/test-utils";
+
+describe("search", function() {
+  it("show empty state");
+  it("set value to search will not trigger search");
+  it("press enter will trigger search");
+  it("validate input. no input, show error message");
+  it("show 10 results per page");
+  it("can navigate to next page");
+  it("show error message if no result");
+  it("show all information required");
+});
