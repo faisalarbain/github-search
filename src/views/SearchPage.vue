e783cfc4976758474cfdcf31e7306970764b0e6f
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-28 23:23:40 +0800

    search form added to page

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <search-form />
     <empty-state />
   </div>
 </template>
 
 <script>
 import EmptyState from "@/components/EmptyState";
+import SearchForm from "@/components/SearchForm";
 export default {
   components: {
-    EmptyState
+    EmptyState,
+    SearchForm
   }
 };
 </script>
