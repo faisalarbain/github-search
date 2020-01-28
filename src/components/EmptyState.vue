cb118b3777b6003c64da8e610064dd172129b7cb
Muhammad Faisal Bin Arba'in <faisal.arbain@gmail.com>
2020-01-29 07:31:44 +0800

    empty state

--- src/components/EmptyState.vue
+++ src/components/EmptyState.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-    show empty state here
+  <div class="hero ">
+    <div class="hero-body">
+      <div style="width:100%;" class="has-text-centered">
+        <icon class="icon is-large has-text-grey-light" name="search"></icon>
+        <p class="title is-2 has-text-grey-light">Search</p>
+        <p class="heading has-text-grey-light">Type your keyword and press "Enter"</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Icon from "./Icon";
+
+export default {
+  components: {
+    Icon
+  }
+};
+</script>
